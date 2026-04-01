8e82b646347a41d8f6f90170f449f0c2f8980e32
divyansh884 <divyanshsharma35403@gmail.com>
2026-04-01 20:03:18 +0530

    gg

--- integrated_system/src/models/face_recognition/config.py
+++ integrated_system/src/models/face_recognition/config.py
@@ -10,5 +10,5 @@ class FaceRecognitionConfig:
     tts_router_url: str = os.getenv("SPEECH_ROUTER_URL", "http://127.0.0.1:8000")
     camera_index: int = int(os.getenv("CAMERA_INDEX", "0"))
     max_camera_index: int = int(os.getenv("MAX_CAMERA_INDEX", "10"))
-    process_every_n_frames: int = 5
+    
 
--- integrated_system/src/models/weapon_detection/config.py
+++ integrated_system/src/models/weapon_detection/config.py
@@ -13,3 +13,4 @@ class WeaponDetectionConfig:
     frame_threshold: int = int(os.getenv("WEAPON_FRAME_THRESHOLD", "5"))
     max_camera_index: int = int(os.getenv("MAX_CAMERA_INDEX", "5"))
     router_url: str = os.getenv("SPEECH_ROUTER_URL", "http://127.0.0.1:8000")
+    process_every_n_frames: int = 5
